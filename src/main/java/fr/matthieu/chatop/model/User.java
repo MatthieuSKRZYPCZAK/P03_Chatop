24363100cb8a9b3acb1c7ac53e97374adb644df8
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-12-04 11:24:46 +0100

    Update User entity to implement UserDetails, changed id type from Integer to Long. Added @Data annotation for automatic generation of getters, setters.

--- src/main/java/fr/matthieu/chatop/model/User.java
+++ src/main/java/fr/matthieu/chatop/model/User.java
@@ -1,20 +1,50 @@
 package fr.matthieu.chatop.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Data
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 
+	@Column(unique = true, nullable = false)
 	private String email;
 
+	@Column(nullable = false)
 	private String password;
 
+	@Column(nullable = false)
 	private String name;
 
+	@CreatedDate
+	@Column(name="created_at")
+	private LocalDateTime createdAt;
 
+	@LastModifiedDate
+	@Column(name="updated_at")
+	private LocalDateTime updatedAt;
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of();
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
 }
