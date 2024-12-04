d8ba59992cd6ecec4bc1f02251975c8f97de04e6
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-12-04 20:05:44 +0100

    Update Message model with entity mapping and fields

--- src/main/java/fr/matthieu/chatop/model/Message.java
+++ src/main/java/fr/matthieu/chatop/model/Message.java
@@ -1,4 +1,24 @@
 package fr.matthieu.chatop.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "messages")
 public class Message {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String message;
+
+	@Column(name="created_at")
+	private LocalDateTime createdAt;
+
+	@Column(name="updated_at")
+	private LocalDateTime updatedAt;
 }
