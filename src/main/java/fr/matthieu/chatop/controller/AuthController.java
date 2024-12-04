1093cd7066cecb4e7b95cee1a80e4b339f1c184e
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-12-04 20:05:07 +0100

    - Added /register endpoint to handle user registration:   - Validates incoming RegisterDTO and registers a new user using UserService. - Updated /login endpoint:   - Authenticates the user using AuthenticationManager.   - Returns a JWT if authentication is successful, or an unauthorized response otherwise. - Added /me endpoint to retrieve authenticated user details using UserService.

--- src/main/java/fr/matthieu/chatop/controller/AuthController.java
+++ src/main/java/fr/matthieu/chatop/controller/AuthController.java
@@ -1,16 +1,19 @@
 package fr.matthieu.chatop.controller;
 
 import fr.matthieu.chatop.dto.AuthenticationDTO;
+import fr.matthieu.chatop.dto.RegisterDTO;
+import fr.matthieu.chatop.model.User;
 import fr.matthieu.chatop.service.JWTService;
+import fr.matthieu.chatop.service.UserService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.*;
 
 import static fr.matthieu.chatop.controller.ApiRoutes.*;
 
@@ -19,24 +22,46 @@ import static fr.matthieu.chatop.controller.ApiRoutes.*;
 public class AuthController {
 
 	public JWTService jwtService;
+    private final UserService userService;
+	private final AuthenticationManager authenticationManager;
 
-	private AuthenticationManager authenticationManager;
-
-	public AuthController(JWTService jwtService) {
+	@Autowired
+	public AuthController(UserService userService, AuthenticationManager authenticationManager, JWTService jwtService) {
 		this.jwtService = jwtService;
+		this.userService = userService;
+		this.authenticationManager = authenticationManager;
 	}
 
-//	@PostMapping(LOGIN_URL)
-//	public String getToken(Authentication authentication) {
-//		return jwtService.generateToken(authentication);
-//	}
-
 	@PostMapping(LOGIN_URL)
-	public Map<String, String> login(@RequestBody AuthenticationDTO authenticationDTO) {
+	public ResponseEntity<Object> login(@RequestBody AuthenticationDTO authenticationDTO) {
 		Authentication authentication = authenticationManager.authenticate(
 				new UsernamePasswordAuthenticationToken(authenticationDTO.username(), authenticationDTO.password())
 		);
 		log.info("result {}", authentication.isAuthenticated());
-		return null;
+		if (authentication.isAuthenticated()) {
+			return ResponseEntity.ok().body(this.jwtService.generate(authenticationDTO.username()));
+		} else {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not authenticated");
+		}
 	}
+
+	@PostMapping(REGISTER_URL)
+	public ResponseEntity<Object> register(@Valid @RequestBody RegisterDTO registerDTO) {
+		log.info("Registering new user");
+		userService.register(registerDTO);
+		return ResponseEntity.ok().body("User registered successfully");
+	}
+
+	@GetMapping(ME_URL)
+	public ResponseEntity<Object> getUser() {
+		System.out.println("ddans me");
+		User user = userService.getAuthenticateUser();
+		try {
+			return ResponseEntity.ok().body(user);
+		} catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error " + e.getMessage());
+		}
+
+	}
+
 }
