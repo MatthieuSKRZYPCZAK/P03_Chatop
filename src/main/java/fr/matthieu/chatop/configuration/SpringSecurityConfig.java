c61183860ab26dbe0efd6a7f81dcfb6621a4576d
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-12-11 17:09:43 +0100

    - Added detailed Javadoc - Updated the security configuration to include route permissions for Swagger API documentation (API_DOCS_URL and SWAGGER_UI_URL)

--- src/main/java/fr/matthieu/chatop/configuration/SpringSecurityConfig.java
+++ src/main/java/fr/matthieu/chatop/configuration/SpringSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,10 +16,14 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
-import static fr.matthieu.chatop.controller.ApiRoutes.*;
+import static fr.matthieu.chatop.common.ApiRoutes.*;
 
+/**
+ * Configuration class for setting up Spring Security.
+ * This class defines the security settings for the application, including authentication, authorization,
+ * and session management.
+ */
 @Configuration
-@EnableWebSecurity
 public class SpringSecurityConfig {
 
 
@@ -40,20 +43,38 @@ public class SpringSecurityConfig {
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers(
 								REGISTER_URL,
-								LOGIN_URL
+								LOGIN_URL,
+								API_DOCS_URL,
+								SWAGGER_UI_URL
 								).permitAll()
 						.anyRequest().authenticated());
 		http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
 
+	/**
+	 * Provides the {@link AuthenticationManager} bean.
+	 * The {@code AuthenticationManager} is used to process authentication requests.
+	 *
+	 * @param authenticationConfiguration The {@link AuthenticationConfiguration} for setting up the manager.
+	 * @return A configured {@link AuthenticationManager}.
+	 * @throws Exception If an error occurs while setting up the authentication manager.
+	 */
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
 	}
 
+	/**
+	 * Provides the {@link AuthenticationProvider} bean.
+	 * The {@code AuthenticationProvider} is responsible for authenticating users by retrieving user details
+	 * and verifying their credentials using the password encoder.
+	 *
+	 * @param userDetailsService The service for loading user details.
+	 * @return A configured {@link AuthenticationProvider}.
+	 */
 	@Bean
-	public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) throws Exception {
+	public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
 		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
 		daoAuthenticationProvider.setUserDetailsService(userDetailsService);
 		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
