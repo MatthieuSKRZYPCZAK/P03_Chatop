4653717c2202c2dbee8ec919d515e08d6d1387ed
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-12-11 17:14:03 +0100

    Add ResponseMessages utility class for standardized API response messages

---
+++ src/main/java/fr/matthieu/chatop/common/ResponseMessages.java
@@ -0,0 +1,22 @@
+package fr.matthieu.chatop.common;
+
+/**
+ * A utility class that defines constant messages used in API responses.
+ * This class centralizes common response messages.
+ */
+public class ResponseMessages {
+
+	private ResponseMessages() {}
+
+	public static final String REGISTER_FAILED = "register failed";
+
+	public static final String EMAIL_ALREADY_IN_USE = "email address already in use";
+
+	public static final String INVALID_CREDENTIALS = "invalid credentials";
+
+	public static final String ACCESS_DENIED = "access denied";
+
+	public static final String UNAUTHORIZED_ACCESS = "unauthorized access";
+
+	public static final String USER_NOT_FOUND = "user not found";
+}
