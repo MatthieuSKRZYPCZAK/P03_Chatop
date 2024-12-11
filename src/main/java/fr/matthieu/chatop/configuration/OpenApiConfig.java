e1e6370004fd5ac1a128cc05cb4df6bb97bd4cb9
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-12-11 21:30:57 +0100

    feat: add OpenAPI configuration for ChâTop API

---
+++ src/main/java/fr/matthieu/chatop/configuration/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package fr.matthieu.chatop.configuration;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+		info = @Info(
+				title = "ChâTop API",
+				version = "1.0",
+				description = "This API allows managing ChâTop",
+				contact = @Contact(
+						name = "Matthieu"
+				)
+		),
+		security = @SecurityRequirement(name = "bearerAuth"),
+		servers = @Server(url = "http://localhost:3001")
+)
+@SecurityScheme(
+		name = "bearerAuth",
+		type = SecuritySchemeType.HTTP,
+		scheme = "bearer",
+		bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
