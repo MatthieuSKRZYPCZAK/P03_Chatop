c640d0f37dde16dec91d172b86cf13a613350ccc
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-12-11 17:08:02 +0100

    Add EnabledWebSecurity annotation

--- src/main/java/fr/matthieu/chatop/ChatopApplication.java
+++ src/main/java/fr/matthieu/chatop/ChatopApplication.java
@@ -3,8 +3,10 @@ package fr.matthieu.chatop;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class ChatopApplication {
 
 	public static void main(String[] args) {
