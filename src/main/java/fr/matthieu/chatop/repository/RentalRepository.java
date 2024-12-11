2d111d0cfb8834bfa8e5fa56cf0363302fb17c9f
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-12-11 21:31:49 +0100

    Add RentalRepository

---
+++ src/main/java/fr/matthieu/chatop/repository/RentalRepository.java
@@ -0,0 +1,9 @@
+package fr.matthieu.chatop.repository;
+
+import fr.matthieu.chatop.model.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RentalRepository  extends JpaRepository<Rental, Long> {
+}
