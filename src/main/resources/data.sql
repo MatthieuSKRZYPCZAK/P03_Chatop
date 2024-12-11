5e80d1dc18b128b82fd635979b0668a2b4bd9445
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-12-11 17:04:52 +0100

    Update sql.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO users (id, created_at, email, name, password, updated_at) VALUES (1, CURRENT_TIMESTAMP,'test@test.fr' , 'MrTest', '$2a$10$YeiT4ydjp6lHWuVpXQh9nO1sid7uqOxgpWne9ufnT8TuCfUwcd9ou', NULL);
+INSERT INTO users (created_at, email, name, password, updated_at, token_version) VALUES (CURRENT_TIMESTAMP,'test@test.fr' , 'MrTest', '$2a$10$YeiT4ydjp6lHWuVpXQh9nO1sid7uqOxgpWne9ufnT8TuCfUwcd9ou', NULL, 1);
