6dd409c96ce1233a0cc1da2b003b87be8d525fbe
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-12-12 11:02:29 +0100

    feat: enhance GlobalExceptionHandler with logging and additional exception handling
    - Added logging using Lombok's @Slf4j annotation for improved debugging and traceability.
    - Introduced handlers for `RentalNotFoundException` and `JwtException` to manage rental not found errors and JWT issues

--- src/main/java/fr/matthieu/chatop/exception/GlobalExceptionHandler.java
+++ src/main/java/fr/matthieu/chatop/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package fr.matthieu.chatop.exception;
 
 import fr.matthieu.chatop.common.ErrorResponse;
 import fr.matthieu.chatop.common.ResponseMessages;
+import io.jsonwebtoken.JwtException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -18,6 +20,7 @@ import java.util.Map;
  * This class handles specific exceptions and returns appropriate HTTP responses with error details.
  * It uses Spring's {@code @ControllerAdvice} to intercept exceptions across the application.
  */
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -31,6 +34,7 @@ public class GlobalExceptionHandler {
 	 */
 	@ExceptionHandler(UserAlreadyExistsException.class)
 	public ResponseEntity<Object> handleUserAlreadyExists(UserAlreadyExistsException e) {
+		log.info("Handling UserAlreadyExistsException - User with the given email already exists.");
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
 	}
 
@@ -44,6 +48,7 @@ public class GlobalExceptionHandler {
 	 */
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		log.info("Handling MethodArgumentNotValidException - Validation failed for method arguments.");
 		Map<String, String> errors = new HashMap<>();
 		ex.getBindingResult().getFieldErrors().forEach(
 				error -> errors.put(error.getField(), error.getDefaultMessage())
@@ -61,8 +66,34 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(BadCredentialsException.class)
 	@ResponseStatus(HttpStatus.UNAUTHORIZED)
 	public ResponseEntity<Object> handleBadCredentials(BadCredentialsException e) {
+		log.info("Handling BadCredentialsException - Invalid credentials provided for authentication. - " + e.getMessage());
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
 				.body(new ErrorResponse(ResponseMessages.INVALID_CREDENTIALS));
 	}
 
+	/**
+	 * Handles the {@link RentalNotFoundException}.
+	 * This exception is thrown when a rental is not found in the system.
+	 *
+	 * @param e The exception instance containing the error message.
+	 * @return A {@link ResponseEntity} with an error message and HTTP status 404 (Not Found).
+	 */
+	@ExceptionHandler(RentalNotFoundException.class)
+	public ResponseEntity<Object> handleRentalNotFoundException(RentalNotFoundException e) {
+		log.info("Handling RentalNotFoundException - Rental not found.");
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage()));
+	}
+
+	/**
+	 * Handles the {@link JwtException}.
+	 * This exception is thrown when there is an issue with JWT processing.
+	 *
+	 * @param e The exception instance containing the error message.
+	 * @return A {@link ResponseEntity} with an error message and HTTP status 401 (Unauthorized).
+	 */
+	@ExceptionHandler(JwtException.class)
+	public ResponseEntity<ErrorResponse> handleJwtException(JwtException e) {
+		log.info("Handling JwtException - Issue with JWT processing.");
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(e.getMessage()));
+	}
 }
