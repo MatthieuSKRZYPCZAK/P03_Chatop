75405cd53f7c0502f301ff778e006822e30e4d6a
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-12-12 11:31:48 +0100

    Add rental creation functionality in RentalService

--- src/main/java/fr/matthieu/chatop/service/RentalService.java
+++ src/main/java/fr/matthieu/chatop/service/RentalService.java
@@ -1,11 +1,14 @@
 package fr.matthieu.chatop.service;
 
 import fr.matthieu.chatop.common.ResponseMessages;
+import fr.matthieu.chatop.dto.CreateRentalDTO;
 import fr.matthieu.chatop.dto.RentalDTO;
 import fr.matthieu.chatop.exception.RentalNotFoundException;
 import fr.matthieu.chatop.mapper.RentalMapper;
 import fr.matthieu.chatop.model.Rental;
+import fr.matthieu.chatop.model.User;
 import fr.matthieu.chatop.repository.RentalRepository;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,11 +23,13 @@ public class RentalService {
 
 	private final RentalRepository rentalRepository;
 	private final RentalMapper rentalMapper;
+	private final UserService userService;
 
 	@Autowired
-	public RentalService(RentalRepository rentalRepository, RentalMapper rentalMapper) {
+	public RentalService(RentalRepository rentalRepository, RentalMapper rentalMapper, UserService userService) {
 		this.rentalRepository = rentalRepository;
 		this.rentalMapper = rentalMapper;
+		this.userService = userService;
 	}
 
 	public List<RentalDTO> getAllRentalsDTO() {
@@ -40,5 +45,12 @@ public class RentalService {
 						String.format(RENTAL_NOT_FOUND, id)
 				));
 		return rentalMapper.toDTO(rental);
+	}
+
+	public boolean createRental(@Valid CreateRentalDTO createRentalDTO) {
+		String picture = "url";
+		User user = userService.getAuthenticateUser();
+		Rental rental = new Rental(createRentalDTO.name(), createRentalDTO.surface(), createRentalDTO.price(), picture, createRentalDTO.description(), user);
+		return true;
 	}
 }
