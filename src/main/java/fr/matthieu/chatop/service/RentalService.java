9a61b41c520c06470e293b32085f491b9eb7ea52
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2024-12-12 17:48:58 +0100

    feat: enhance RentalService with rental creation and update functionality

--- src/main/java/fr/matthieu/chatop/service/RentalService.java
+++ src/main/java/fr/matthieu/chatop/service/RentalService.java
@@ -1,14 +1,14 @@
 package fr.matthieu.chatop.service;
 
-import fr.matthieu.chatop.common.ResponseMessages;
 import fr.matthieu.chatop.dto.CreateRentalDTO;
 import fr.matthieu.chatop.dto.RentalDTO;
 import fr.matthieu.chatop.exception.RentalNotFoundException;
+import fr.matthieu.chatop.exception.UnauthorizedException;
 import fr.matthieu.chatop.mapper.RentalMapper;
 import fr.matthieu.chatop.model.Rental;
 import fr.matthieu.chatop.model.User;
 import fr.matthieu.chatop.repository.RentalRepository;
-import jakarta.validation.Valid;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 import static fr.matthieu.chatop.common.ResponseMessages.RENTAL_NOT_FOUND;
+import static fr.matthieu.chatop.common.ResponseMessages.UNAUTHORIZED_ACCESS;
 
 @Slf4j
 @Service
@@ -24,12 +25,15 @@ public class RentalService {
 	private final RentalRepository rentalRepository;
 	private final RentalMapper rentalMapper;
 	private final UserService userService;
+	private final UploadService uploadService;
+
 
 	@Autowired
-	public RentalService(RentalRepository rentalRepository, RentalMapper rentalMapper, UserService userService) {
+	public RentalService(RentalRepository rentalRepository, RentalMapper rentalMapper, UserService userService, UploadService uploadService) {
 		this.rentalRepository = rentalRepository;
 		this.rentalMapper = rentalMapper;
 		this.userService = userService;
+		this.uploadService = uploadService;
 	}
 
 	public List<RentalDTO> getAllRentalsDTO() {
@@ -40,17 +44,42 @@ public class RentalService {
 	}
 
 	public RentalDTO getRentalDTOById(Long id) {
-		Rental rental = rentalRepository.findById(id)
-				.orElseThrow(() -> new RentalNotFoundException(
-						String.format(RENTAL_NOT_FOUND, id)
-				));
+		Rental rental = getRentalById(id);
 		return rentalMapper.toDTO(rental);
 	}
 
-	public boolean createRental(@Valid CreateRentalDTO createRentalDTO) {
-		String picture = "url";
+	@Transactional
+	public void createRental(CreateRentalDTO createRentalDTO) {
 		User user = userService.getAuthenticateUser();
+		String picture = uploadService.storePicture(createRentalDTO.picture(), user.getName());
 		Rental rental = new Rental(createRentalDTO.name(), createRentalDTO.surface(), createRentalDTO.price(), picture, createRentalDTO.description(), user);
-		return true;
+		rentalRepository.save(rental);
+	}
+
+	private Rental getRentalById(Long id) {
+		return rentalRepository.findById(id)
+				.orElseThrow(() -> new RentalNotFoundException(
+						String.format(RENTAL_NOT_FOUND, id)
+				));
+	}
+
+	public Rental checkOwner(Long id) {
+		User user = userService.getAuthenticateUser();
+		Rental rental = getRentalById(id);
+		if(rental.getOwner().equals(user)) {
+			return rental;
+		}
+		throw new UnauthorizedException(UNAUTHORIZED_ACCESS);
+	}
+
+	@Transactional
+	public void UpdateRental(Rental rental, CreateRentalDTO createRentalDTO) {
+
+		rental.setName(createRentalDTO.name());
+		rental.setSurface(createRentalDTO.surface());
+		rental.setPrice(createRentalDTO.price());
+		rental.setDescription(createRentalDTO.description());
+
+		rentalRepository.save(rental);
 	}
 }
